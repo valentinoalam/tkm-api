314c52e6cbcb852cf2a93b2007ecccbda1dcd785
Ichikyube <ichikyube@gmail.com>
2024-08-28 09:31:02 +0700

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,12 +3,19 @@ module.exports = {
       {
         name: 'nestjs-app',
         script: 'dist/main.js',
-        instances: 'max',
-        exec_mode: 'cluster',
+        instances: 1,
+        exec_mode: 'fork',
         env: {
           NODE_ENV: 'production',
           PORT: 3000,
         },
+        // Optional configurations (consider adding):
+        error_file: 'logs/err.log', // Path to the error log file
+        out_file: 'logs/out.log', // Path to the output log file
+        merge_logs: true, // Merge logs into a single file (optional)
+        max_restarts: 5, // Maximum number of restarts in case of errors
+        watch: false, // Disable file watching in production (optional)
+        time: false,
       },
     ],
   };
--- src/features/google/google.controller.ts
+++ src/features/google/google.controller.ts
@@ -1,65 +1,12 @@
-import { Controller, Get, HttpException, HttpStatus, Inject, Param, Res } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, Param, Res } from '@nestjs/common';
 import { GoogleService } from './google.service';
-import { drive_v3 } from 'googleapis';
 import { Response } from 'express';
-import { Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
-import { PassThrough } from 'stream';
 @Controller('google')
 export class GoogleController {
-  constructor(private readonly googleService: GoogleService, @Inject(CACHE_MANAGER) private cacheManager: Cache) {}
-
-  @Get('drive')
-  async getDriveApi() {
-    //await this.googleService.authenticate(); // Ensure authentication
-    const drive = await this.googleService.getDriveApi();
-    // https://docs.google.com/spreadsheets/d/1uNwEa_HMegXTBq67QzqGjD6udMwkeqjX8C4I-oZhBS0/edit?usp=drive_link
-    //categori:https://docs.google.com/spreadsheets/d/1uNwEa_HMegXTBq67QzqGjD6udMwkeqjX8C4I-oZhBS0/edit?usp=sharing
-    // galeri:https://drive.google.com/drive/folders/1UMqhmCsA5fSmU8euwKjcCPjJfBqM4pjm?usp=drive_link
-    const arr = [
-      '1OJtHQdWEZDb1GAbIxvcuLQOGVoiRti5p',
-      '1s2d6gtWf-so5gyo1rrPlU6EICczyyYCF',
-      '13vlDfBvUOSdIaZFQjTx0tkjKU09m8yQj',
-      '1GA6APNnxZAwZrHmS4YuGwyEHP6khf7j3'
-    ]
-    try {
-      console.log(drive)
-
-      let res;
-      for (let i=0;i<arr.length;i++) {
-        const listParams: drive_v3.Params$Resource$Files$List = {
-          q: `${arr[i]} in parents`, // Search for files within the specific folder
-          fields: 'files(name, mimeType)', // Only fetch specific file properties
-        }
-        res = drive.files.list(listParams);
-        console.log(res.data)
-      }
-      return res;
-    } catch (error) {
-      console.error(error);
-      throw error; // Re-throw for handling in the calling code
-    }
-    // Use the 'drive' API instance to make API calls
-  }
-
-  // @Get('/:spreadsheetId/:range')
-  // async getSpreadsheetData(@Param('spreadsheetId') spreadsheetId: string, @Param('range') range: string) {
-  //   //await this.googleService.authenticate();
-  //   const data = await this.googleService.getSpreadsheetData(spreadsheetId, range);
-  //   return data;
-  // }
-  // @Get('itiqaf-sheet')
-  // async getMastersheetData() {
-  //   //await this.googleService.authenticate();
-  //   const sheet = await this.googleService.getMastersheetData();
-  //   const rows = await sheet.getRows()
-  //   const participants = rows.filter((row)=>row.get('Nama Lengkap')).map((row, i) => ({ id: i, name: row.get('Nama Lengkap'), sex: row.get('Jenis Kelamin') }));
-  //   return participants;
-  // }
+  constructor(private readonly googleService: GoogleService) {}
 
   @Get('foto-nota')
   async getFotoNotaData() {
-    //await this.googleService.authenticate(); // Ensure authentication
-
     const drive = await this.googleService.getDriveApi(); // Assume this method returns the Drive API client
     const gallery = await this.googleService.getFotoNotaData(drive);
 
@@ -69,14 +16,12 @@ export class GoogleController {
 
   @Get('kas-kecil')
   async getKasKecilData() {
-    //await this.googleService.authenticate();
     const jurnal = await this.googleService.getKasKecilData();
 
     return jurnal;
   }
   @Get('kategori-entry')
   async syncKasKecilKategori() {
-    //await this.googleService.authenticate();
     const categories = await this.googleService.syncKasKecilKategori();
 
     return categories;
@@ -85,16 +30,6 @@ export class GoogleController {
   
   @Get('image/:fileId')
   async getImage(@Param('fileId') fileId: string, @Res() res: Response) {
-    // const cacheKey = 'key';
-    // const cachedImage = await this.cacheManager.get(cacheKey);
-
-    // if (cachedImage) {
-    //   console.log("myamya")
-    //   const cachedStream = new PassThrough();
-    //   cachedStream.end(cachedImage);
-    //   res.setHeader('Content-Type', 'image/jpeg');
-    //   return cachedStream.pipe(res); // Return the cached image
-    // }
 
     try {
       //await this.googleService.authenticate();
@@ -102,31 +37,6 @@ export class GoogleController {
       const fileStream = await this.googleService.getPhotoDetail(drive, fileId);
       res.setHeader('Content-Type', 'image/jpeg');
       fileStream.pipe(res);
-      // // Cache the image stream
-      // this.cacheManager.set(cacheKey, file, 600);
-      // const chunks = [];
-      // fileStream.on('data', chunk => {
-      //   chunks.push(chunk);
-      // });
-
-      // fileStream.on('end', async () => {
-      //   const buffer = Buffer.concat(chunks);
-      //   try {
-      //     await this.cacheManager.set(cacheKey, buffer, 600);
-      //     console.log("Cached image successfully");
-      //   } catch (cacheError) {
-      //     console.error('Failed to cache image:', cacheError);
-      //   }
-      //   const stream = new PassThrough();
-      //   stream.end(buffer);
-      //   res.setHeader('Content-Type', 'image/jpeg');
-      //   stream.pipe(res);
-      // });
-
-      // fileStream.on('error', error => {
-      //   console.error('Error streaming file:', error);
-      //   res.status(500).send('Error streaming file');
-      // });
 
     } catch (error) {
       console.error(error);
