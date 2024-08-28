95955881a15c30006c74c416442c010a73ae1210
Ichikyube <ichikyube@gmail.com>
2024-08-28 09:41:39 +0700

    ecosystemi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,34 +4,24 @@ import { CacheModule, CacheStoreFactory } from '@nestjs/cache-manager';
 import { ScheduleModule } from '@nestjs/schedule';
 import { DatabaseModule } from '../core/database/database.module';
 import { join } from 'path';
-
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { HealthModule } from './health/health.module';
 import { ConfigModule } from '@nestjs/config';
 import configuration from '@core/config/configuration';
 import { ConfigValidator } from '@/core/config/validator/config.validator';
-// import { EventEmitterModule } from '@nestjs/event-emitter';
-// import { BroadcastService } from '@feat/sse/broadcast.service';
-// import { SseService } from '@feat/sse/sse.service';
-
-// import { SseController } from '@feat/sse/sse.controller';
 import { utilities as nestWinstonModuleUtilities, WinstonModule } from 'nest-winston';
 import * as winston from 'winston';
 import { LoggedMiddleware } from '@common/middlewares/logged.middleware';
 import { FeaturesModule } from '@/features/features.module';
-import redisStore from 'cache-manager-redis-store';
-// import type { RedisClientOptions } from 'redis';
-// import { appConfigValidationSchema } from '@core/config/config.schema';
+
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [configuration],
-      // validationSchema: appConfigValidationSchema,
       validate: ConfigValidator,
       isGlobal: true,
-      // envFilePath: path.resolve(__dirname, '../../.env'), // if you want to specify the path to env file
     }),
     HealthModule,
     DatabaseModule,
@@ -49,62 +39,15 @@ import redisStore from 'cache-manager-redis-store';
             }),
           ),
         }),
-        // other transports...
       ],
     //   // other options
     }),
-    // EventEmitterModule.forRoot({
-    //   // set this to `true` to use wildcards
-    //   wildcard: false,
-    //   // the delimiter used to segment namespaces
-    //   delimiter: '.',
-    //   // set this to `true` if you want to emit the newListener event
-    //   newListener: false,
-    //   // set this to `true` if you want to emit the removeListener event
-    //   removeListener: false,
-    //   // the maximum amount of listeners that can be assigned to an event
-    //   maxListeners: 10,
-    //   // show event name in memory leak message when more than maximum amount of listeners is assigned
-    //   verboseMemoryLeak: false,
-    //   // disable throwing uncaughtException if an error event is emitted and it has no listeners
-    //   ignoreErrors: false,
-    // }),
     // ScheduleModule.forRoot(),
-    CacheModule.register({
-      isGlobal: true,
-      redisStore,
-      host: 'localhost',
-      port: 6379,
-      ttl: 600,
-    }),
+
     // ServeStaticModule.forRoot({
     //   rootPath: join(__dirname, '..', 'upload'),
     //   serveRoot: '/img/',
     // }),
-    // GraphQLModule.forRootAsync({
-    //   useFactory: async (appConfig: AppConfigService) => ({
-    //     installSubscriptionHandlers: true,
-    //     buildSchemaOptions: {
-    //       numberScalarMode: 'integer',
-    //     },
-    //     sortSchema: appConfig.graphqlSortSchema,
-    //     autoSchemaFile: appConfig.graphqlSchemaDestination,
-    //     debug: appConfig.graphqlDebug,
-    //     playground: appConfig.graphqlPlaygroundEnabled,
-    //     context: ({ req }) => ({ req }),
-    //     formatError: (error: GraphQLError) => {
-    //       const exception = error.extensions?.exception?.response;
-    //       const logger = new Logger('GraphQLError');
-    //       logger.error(JSON.stringify(error));
-
-    //       return {
-    //         message: exception?.message || error.message || 'INTERNAL_SERVER_ERROR',
-    //         code: exception?.code || error.extensions?.response?.statusCode || 500,
-    //         data: exception?.data || {},
-    //         info: exception?.code || error.extensions?.response?.message || '',
-    //       };
-    //     },
-    //   }),
     //   inject: [AppConfigService],
     //   imports: [AppConfigModule],
     // }),
