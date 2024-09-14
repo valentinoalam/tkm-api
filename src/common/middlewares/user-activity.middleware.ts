8aed78e64cc114effc99530897c6af0a1cfba85b
Ichikyube <ichikyube@gmail.com>
2024-09-14 13:18:55 +0700

    drsgr

--- src/common/middlewares/user-activity.middleware.ts
+++ src/common/middlewares/user-activity.middleware.ts
@@ -1,17 +1,18 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { UsersService } from '@/features/users/services/users.service';
+import { User } from '@/features/users/entities';
 
 @Injectable()
 export class UserActivityMiddleware implements NestMiddleware {
   constructor(private usersService: UsersService) {}
 
   async use(req: Request, res: Response, next: NextFunction) {
-    const userId = req.user?.id; // Assuming you have user ID in request
-    console.log('Request logged:', req.method, req.originalUrl);
-    console.log(req.user)
-    if (userId) {
-      await this.usersService.updateLastActive(userId);
+    const user = req.user as User; // Ensure that you cast it correctly
+    if (user && user.id) {
+      // User ID is now available
+      console.log('User ID:', user.id);
+      await this.usersService.updateLastActive(user.id);
     }
     next();
   }
